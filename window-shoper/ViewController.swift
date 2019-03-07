63fe82a462a33c0075f7bb94322e55108ce4d221
Doombringer <royalthings2019@gmail.com>
2019-03-07 23:26:30 +0200

    first dev

---
+++ window-shoper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shoper
+//
+//  Created by Dima on 3/7/19.
+//  Copyright © 2019 Dima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
